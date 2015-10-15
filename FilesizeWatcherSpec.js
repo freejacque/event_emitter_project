57157320e3388a2948a59b2d9e09ffb11a910e18
freejacque <freejacque@gmail.com>
2015-10-14 22:27:00 -0400

    started a really long describe test

--- FilesizeWatcherSpec.js
+++ FilesizeWatcherSpec.js
@@ -2,3 +2,21 @@
 
 var FilesizeWatcher = require('./FilesizeWatcher');
 var exec = require('child_process').exec;
+
+describe('FilesizeWatcher', function ({
+
+  var watcher;
+
+  afterEach(function() {
+    watcher.stop();
+  });
+
+  it('should fire a "grew" event when the file grew in size', function(done) {
+
+    var path = '/var/tmp/filesizewatcher.test';
+    exec('rm -f ' + path + ' ; touch ' + path, function() {
+      watcher = new FilesizeWatcher(path);
+    })
+  })
+
+}))
