acf5164c08fb738628c403f5b49941155a5b60a4
freejacque <freejacque@gmail.com>
2015-10-18 19:41:14 -0400

    made a callback for increased file size

--- FilesizeWatcher.js
+++ FilesizeWatcher.js
@@ -11,4 +11,19 @@ var FilesizeWatcher = function(path) {
     self.callbacks['error']('Path does not start with a slash');
     return;
   }
+
+  fs.stat(path, function(err, status) {
+    self.lastfilesize = stats.size;
+  });
+
+  self.interval = setInterval(
+    function() {
+      fs.stat(path, function(err, stats) {
+        if(stats.size > self.lastfilesize) {
+          self.callbacks['grew'](stats.size - self.lastfilesize);
+          self.lastfilesize = stats.size;
+        }
+      })
+    }
+    )
 }
