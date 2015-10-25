9eff895f7a530572cd8842d9edef7ea025db123c
freejacque <freejacque@gmail.com>
2015-10-25 18:37:50 -0400

    created getUserName function

--- client2.js
+++ client2.js
@@ -4,3 +4,13 @@ var request = require('request');
 var async = require('async');
 
 var name, status;
+
+var getUserName = function(callback) {
+  request.get('http://localhost:8080/getUserName?id=1234',
+    function(err, res, body) {
+      var result = JSON.parse(body);
+      callback(err, result.value);
+    });
+};
+
+
