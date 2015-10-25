eec96bff692cc53223fc7af76d69ffc245cffa3f
freejacque <freejacque@gmail.com>
2015-10-25 18:43:08 -0400

    added a comment

--- client2.js
+++ client2.js
@@ -5,6 +5,8 @@ var async = require('async');
 
 var name, status;
 
+
+// requests wrapped in named functions
 var getUserName = function(callback) {
   request.get('http://localhost:8080/getUserName?id=1234',
     function(err, res, body) {
@@ -21,4 +23,6 @@ var getUserStatus = function(callback) {
     });
 };
 
-
+async.parallel([getUserName, getUserStatus], function(err, results) {
+  console.log('The status of user ', results[0], ' is', results[1]);
+});
