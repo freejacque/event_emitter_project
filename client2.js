d5f4639944c72bc36d75b92cc331c25d448c183e
freejacque <freejacque@gmail.com>
2015-10-25 18:40:29 -0400

    created getUserStatus function

--- client2.js
+++ client2.js
@@ -13,4 +13,12 @@ var getUserName = function(callback) {
     });
 };
 
+var getUserStatus = function(callback) {
+  request.get('http://localhost:8080/getUserStatus?id=1234',
+    function(err, res, body) {
+      var result = JSON.parse(body);
+      callback(err, result.value);
+    });
+};
+
 
