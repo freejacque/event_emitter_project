f7f6f2f923deb8b8dd45bb9df496ea275d058131
freejacque <freejacque@gmail.com>
2015-10-26 18:34:44 -0400

    removed artificial slowness from server

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ http.createServer(function(request, response) {
     'value': Math.floor(Math.random() * 100)
   };
 
-  setTimeout(function() {
     response.writeHead(200, {"Content-Type": "application/json"});
     response.end(JSON.stringify(result));
-  }, 2000 + Math.floor(Math.random() & 1000));
 
 }).listen(
   8080,
