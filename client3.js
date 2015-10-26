e127ea254a52cf710461fde0dc51d1d427bc84e9
freejacque <freejacque@gmail.com>
2015-10-26 18:47:09 -0400

    added two functions in the series

--- client3.js
+++ client3.js
@@ -3,3 +3,22 @@
 var request = require('request');
 var async = require('async');
 
+var url = 'http://localhost:8080/';
+
+async.series([
+
+  function(callback) {
+    request.get(url + 'getUserName?id=1234', function(err, res, body) {
+      callback(null, 'Name: ' + JSON.parse(body).value);
+    });
+  },
+
+  function(callback) {
+    request.get(url + 'getUserStatus?id=1234', function(err, res, body) {
+      callback(null, 'Status: ' + JSON.parse(body).value);
+    });
+  },
+
+
+
+])
