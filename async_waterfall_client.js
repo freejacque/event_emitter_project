7c43752b60ca15864fb2dfc1802a76243e621f89
freejacque <freejacque@gmail.com>
2015-10-28 19:57:54 -0400

    added first async function

--- async_waterfall_client.js
+++ async_waterfall_client.js
@@ -1,6 +1,17 @@
 'use strict';
 
-
 var request = require('request');
 var async = require('async');
 
+var url = 'http://localhost:8080/';
+
+async.waterfall([
+
+  function(callback) {
+    request.get(url + 'getSessionId', function(err, res, body) {
+      callback(null, JSON.parse(body).value);
+    });
+  },
+
+
+])
