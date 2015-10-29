0b97ddfa81346dccc2dfe98e7d3461253068c0cf
freejacque <freejacque@gmail.com>
2015-10-29 19:30:00 -0400

    added a comment

--- async_waterfall_client.js
+++ async_waterfall_client.js
@@ -5,30 +5,33 @@ var async = require('async');
 
 var url = 'http://localhost:8080/';
 
+// form: async.waterfall(arrayOfAsyncFunctions, callbackFunction)
+// callback is passed as the last argument for each step function
 async.waterfall([
 
-  function(callback) {
-    request.get(url + 'getSessionId', function(err, res, body) {
-      callback(null, JSON.parse(body).value);
-    });
-  },
+    function(callback) {
+      request.get(url + 'getSessionId', function(err, res, body) {
+        callback(null, JSON.parse(body).value);
+      });
+    },
 
-  function(sId, callback) {
-    request.get(url + 'getUserId?sessionId=' + sId, function(err, res, body) {
-      callback(null, sId, JSON.parse(body).value);
-    });
-  },
+    function(sId, callback) {
+      request.get(url + 'getUserId?sessionId=' + sId, function(err, res, body) {
+        callback(null, sId, JSON.parse(body).value);
+      });
+    },
 
-  function(sId, uId, callback) {
-    request.get(url + 'getUserName?userId=' + uId, function(err, res, body) {
-      callback(null, JSON.parse(body).value, sId)
-    })
-  }
+    function(sId, uId, callback) {
+      request.get(url + 'getUserName?userId=' + uId, function(err, res, body) {
+        callback(null, JSON.parse(body).value, sId)
+      })
+    }
+
+  ],
 
   function(err, name, sId) {
     console.log('Name: ', name);
     console.log('SessionID: ', sId);
   }
 
-
-])
+);
